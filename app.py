47d80c3f0434581bff8544cc8f6b730cf15a42d0
Uzairf4665 <113177761+Uzairf4665@users.noreply.github.com>
2024-01-22 17:27:37 +0000

    complete the site
    site has been completed

--- app.py
+++ app.py
@@ -1,8 +1,40 @@
-from flask import Flask, render_template
+from flask import Flask, render_template,jsonify
 app = Flask(__name__)
+Jobs = [
+  {
+    'id': 1,
+    'title': 'Data Analyst',
+    'location': 'Bengaluru, India',
+    'salary': 'Rs. 10,00,000'
+  },
+  
+  {
+    'id': 2,
+    'title': 'Cyber Security Analyst',
+    'location': 'Dubai, UAE',
+    'salary': 'Dirahm. 10,000'
+  },
+  
+  {
+    'id': 3,
+    'title': 'FrontEnd Engineer',
+    'location': 'Remote'
+  },
+  
+  {
+    'id': 4,
+    'title': 'BackEnd Engineer',
+    'location': 'Dehli, India',
+    'salary': 'Rs. 5,00,000'
+  },
+]
 @app.route('/')
+
 def hello():
-  return render_template('home.html')
+  return render_template('home.html',Jobs =Jobs, company_name = 'Jovian')
+@app.route('/jobs')
+def joblists():
+  return jsonify(Jobs)
 
 if __name__ =='__main__':
   app.run(host='0.0.0.0', debug=True)
